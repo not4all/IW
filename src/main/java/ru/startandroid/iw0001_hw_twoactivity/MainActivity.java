e04e33df94e026b5982c6d6ddccb7fadfb443141
Ivan Treschanovich <not4allqq@gmail.com>
2021-11-29 12:19:01 +0300

    initial

---
+++ src/main/java/ru/startandroid/iw0001_hw_twoactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.startandroid.iw0001_hw_twoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
